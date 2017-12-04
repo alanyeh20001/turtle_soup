9838aa17496d389c0d2bdd87a62ba72b820e7637
alanyeh20001 <alanyeh20001@hotmail.com>
2017-12-04 16:00:29 +0800

    No assets & helper when controller generator

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,8 @@ module TurtleSoup
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators.assets = false
+    config.generators.helper = false
   end
 end

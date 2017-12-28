722507d1dcfa8df459df70277b7585dacc318db7
alanyeh20001 <alanyeh20001@hotmail.com>
2017-12-28 16:54:32 +0800

    Configure soup with aasm

--- app/models/soup.rb
+++ app/models/soup.rb
@@ -1,4 +1,6 @@
 class Soup < ApplicationRecord
+  include AASM
+
   belongs_to :user
 
   has_many :favorites
@@ -8,6 +10,24 @@ class Soup < ApplicationRecord
   validates :title, :description, presence: true
 
   after_update_commit :broadcast_soup, if: :soup_finished?
+
+  aasm column: :state do
+    state :active, initial: true
+    state :pending
+    state :finished
+
+    event :pause do
+      transitions from: :active, to: :pending
+    end
+
+    event :finish do
+      transitions from: :active, to: :finished
+    end
+
+    event :continue do
+      transitions from: :pending, to: :active
+    end
+  end
 
   private
 

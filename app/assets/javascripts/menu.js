bdf08ac74b4f95bce1572a438e09e63882132c77
alanyeh20001 <alanyeh20001@hotmail.com>
2017-12-29 11:23:56 +0800

    Fix welcome/index when menu open

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -3,12 +3,14 @@ $(function() {
       navbar = $("nav"),
       mainContent = $(".main-content"),
       overlay = $("#overlay"),
+      background = $("#welcome .background"),
       state = "hidden";
 
   menuButton.on("click", function() {
     if (state === "hidden") {
       navbar.addClass("menu-open");
       mainContent.addClass("main-content-open");
+      background.addClass("background-open");
       overlay.addClass("overlay-open");
       state = "open";
     }
@@ -19,6 +21,7 @@ $(function() {
       var $this = this;
       navbar.removeClass("menu-open");
       mainContent.removeClass("main-content-open");
+      background.removeClass("background-open");
       $($this).removeClass("overlay-open");
       state = "hidden";
     }

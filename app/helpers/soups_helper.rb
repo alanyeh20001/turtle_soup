41820e8eac3cbb2a20a8932ff5c50a4c2002c850
alanyeh20001 <alanyeh20001@hotmail.com>
2018-01-05 02:01:41 +0800

    Format the description of soup

--- app/helpers/soups_helper.rb
+++ app/helpers/soups_helper.rb
@@ -6,4 +6,8 @@ module SoupsHelper
   def render_soup_state(soup)
     soup.finished? ? "finished" : soup.pending? ? "pending" : ""
   end
+
+  def format_description(soup)
+    simple_format(strip_tags(soup.description))
+  end
 end
